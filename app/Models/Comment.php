12f72688eead6a101f02668d7448e3bf9fba6077
lalremruatakhiangte <lalremruatakhiangte656@gmail.com>
2025-06-16 14:39:04 +0530

    Describe your changes

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -31,8 +31,8 @@ class Comment extends Model
         return $this->hasMany(Comment::class, 'parent_id');
     }
 
-    public function user()
+     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id', 'id'); // Ensure you're matching user_id in author with the id in users table
     }
 }

5bae0712b47cb5f9bb63da52f8d1c1e1ebc17403
lalremruatakhiangte <lalremruatakhiangte656@gmail.com>
2025-07-29 17:10:51 +0530

    update

--- app/Http/Controllers/PreviewController.php
+++ app/Http/Controllers/PreviewController.php
@@ -43,13 +43,13 @@ class PreviewController extends Controller
         // Set default thumbnail if empty or null
         if ($type === 'listen') {
             // For Audio, check if it has a thumbnail, otherwise fallback to default image
-            $thumbnail = $record->thumbnail ?? $record->cover_image_url ?? 'https://cdn.zostream.in/Normal/Vanneihtluanga/Vanneihtluanga%20coverpg.jpg';
+            $thumbnail = $record->thumbnail ?? 'https://cdn.zostream.in/Normal/Vanneihtluanga/Vanneihtluanga%20coverpg.jpg';
         } elseif ($type === 'watch') {
             // For Video, check if it has a thumbnail, otherwise fallback to default image
-            $thumbnail = $record->thumbnail ?? $record->cover_image_url ?? 'https://cdn.zostream.in/Normal/Vanneihtluanga/Vanneihtluanga%20coverpg.jpg';
+            $thumbnail = $record->thumbnail ?? 'https://cdn.zostream.in/Normal/Vanneihtluanga/Vanneihtluanga%20coverpg.jpg';
         } else {
             // Default for other types (e.g., Article or User)
-            $thumbnail = $record->thumbnail ?? $record->profile_image_url ?? 'https://cdn.zostream.in/Normal/Vanneihtluanga/Vanneihtluanga%20coverpg.jpg';
+            $thumbnail = $record->cover_image_url ?? 'https://cdn.zostream.in/Normal/Vanneihtluanga/Vanneihtluanga%20coverpg.jpg';
         }
 
         // Check for the author and append it to the title

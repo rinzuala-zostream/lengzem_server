17a72e81890a9a5cb17b73f82d7aaeaad4da5ec4
remruatakells <lalremruatakhiangte656@gmail.com>
2025-12-08 12:29:39 +0530

    update

--- app/Models/AdMedia.php
+++ app/Models/AdMedia.php
@@ -11,6 +11,8 @@ class AdMedia extends Model
 
     protected $fillable = ['ad_id', 'media_url', 'media_type'];
 
+    public $timestamps = false;
+
     public function ad()
     {
         return $this->belongsTo(Ad::class);

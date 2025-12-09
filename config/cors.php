b728fce1e9e4f6104c6b2b2cfd1719a96387e776
remruatakells <lalremruatakhiangte656@gmail.com>
2025-12-09 17:30:36 +0530

    update

--- config/cors.php
+++ config/cors.php
@@ -2,22 +2,24 @@
 
 return [
 
-    'paths' => ['api/*', 'content/*'],  // the routes you want CORS enabled for
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],         // allow all HTTP methods
+    'allowed_methods' => ['*'],
 
     'allowed_origins' => [
         'https://app.lengzem.in',
         'https://lengzem.in'
-    ],  // allow your frontend origin
+    ],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],         // allow all headers
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,    // true if you use cookies/auth headers
+    'supports_credentials' => true,
+
 ];
+

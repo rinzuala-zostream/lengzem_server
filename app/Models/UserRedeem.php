a4c8b3ab889f0855f22fe205b6100968a08cef2a
Lengzem <lengzemaizawl@gmail.com>
2026-01-19 10:56:01 +0530

    redeem

---
+++ app/Models/UserRedeem.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class UserRedeem extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_redeems';
+
+    protected $fillable = [
+        'user_id',
+        'redeem_id',
+        'apply_date',
+    ];
+
+    protected $casts = [
+        'apply_date' => 'datetime',
+    ];
+
+    /**
+     * User who applied the redeem
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Redeem item
+     */
+    public function redeem()
+    {
+        return $this->belongsTo(Redeem::class);
+    }
+}

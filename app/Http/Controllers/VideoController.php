d1316575c2269dee6fe0aaa0c3261d1fc19118ec
Lengzem <lengzemaizawl@gmail.com>
2026-02-02 16:47:33 +0530

    test

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -54,7 +54,7 @@ class VideoController extends Controller
             if (!$userId) {
                 return response()->json([
                     'status' => false,
-                    'message' => $e->getMessage(),
+                    'message' => 'Please log in to access premium content.',
                 ], 401);
             }
 
